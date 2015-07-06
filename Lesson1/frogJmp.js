6e63abf29166970c30c16eefb18a85ee7c1536fe
mayacode <mmiarecka@gmail.com>
2015-07-06 11:24:33 +0200

    Codility - JavaScript solutions for Lesson 1
    TapeEquilibrium
    
    PermMissingElem
    
    FrogJmp

---
+++ Lesson1/frogJmp.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Maja Miarecki
+ *
+ * FrogJmp
+ * lesson link: https://codility.com/demo/results/demoC8ZHS6-93H/
+ *
+ */
+
+function solution(X, Y, D) {
+    return Math.ceil((Y - X) / D);
+}
