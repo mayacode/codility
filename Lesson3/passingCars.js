af2a8ed232fd1ec0116949ce1cece9a61262094f
mayacode <mmiarecka@gmail.com>
2015-07-11 17:03:08 +0200

    Lesson 3 - PassingCars JavaScript solution

---
+++ Lesson3/passingCars.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Maja Miarecki
+ * PassingCars
+ * lesson link: https://codility.com/programmers/lessons/2
+ *
+ */
+
+function solution(A) {
+    var len = A.length, i, sum = 0, pairs = 0;
+    var start = A.indexOf(0);
+    for (i = start; i < len; i++) {
+        var e = A[i];
+        if (e === 0) {
+            pairs++;
+        } else {
+            sum += pairs;
+            if (sum > 1000000000) return -1;
+        }
+    }
+    return sum;
+}
