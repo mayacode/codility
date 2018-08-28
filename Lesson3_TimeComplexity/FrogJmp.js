ea6f48cc2ec67da8fdabd87279e7826fef85029c
Maja Miarecki <mmiarecka@gmail.com>
2018-08-28 10:20:30 +0200

    solution for https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/

---
+++ Lesson3_TimeComplexity/FrogJmp.js
@@ -0,0 +1,9 @@
+/**
+ * Maja Miarecki codility solution
+ * https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
+ */
+
+function solution(X, Y, D) {
+  return Math.ceil((Y - X) / D);
+}
+
